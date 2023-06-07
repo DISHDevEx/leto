2ef4d7d6113d393ecb046274b35ace2dba5d1025
JamesF2440 <James.F2440@gmail.com>
2023-06-07 14:16:30 -0600

    Init Commit for James Fagan. Has basic meta data processing

--- aEye/video.py
+++ aEye/video.py
@@ -1,5 +1,21 @@
 class Video:
-    def __init__(self,file) -> None:
+    def __init__(self,file,name:str, codec: str, width: int, height: int, duration: float, frames: int) -> None:
         self.file = file
         self.meta_data = 'insert by James'
-    
+        self.name = name
+        self.codec = codec
+        self.width = int(width)
+        self.height = int(height)
+        self.duration = float(duration)
+        self.frames = int(frames)
+        self.fps = self.frames / self.duration
+
+    def getSize(self):
+        print("Video Resolution:", self.width, 'x', self.height)
+
+    def getVideoDetails(self):
+        print("Metadata for video: " + self.name)
+        self.getSize()
+        print("Encoding Format:", self.codec, "Duration (s):", self.duration, "with a total of", self.frames,
+              "frames! (" + str(self.fps) + " FPS)")
+
