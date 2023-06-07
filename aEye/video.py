2f1d259b5612764e181a478bdf3916a26a74620d
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-06-07 14:39:28 -0600

    test w/ james

--- aEye/video.py
+++ aEye/video.py
@@ -2,21 +2,27 @@ import cv2
 import numpy as np
 
 class Video:
-<<<<<<< HEAD
     """
     @:constructor file
         file is the path name for the file
     """
 
-    def __init__(self,file):
-        """
-
-        """
+    def __init__(self,self,file,name:str, codec: str, width: int, height: int, duration: float, frames: int) -> None:
         self.file = file
+        self.meta_data = 'insert by James'
+        self.name = name
+        self.codec = codec
+        self.width = int(width)
+        self.height = int(height)
+        self.duration = float(duration)
+        self.frames = int(frames)
+        self.fps = self.frames / self.duration
         self.meta_data = 'insert by James'
         self.cap = cv2.VideoCapture(file)
         if self.cap is None:
             print("Error opening video stream or file")
+
+
 
 
     def resize(self, x_ratio, y_ratio):
@@ -42,17 +48,7 @@ class Video:
         """
         this method will extract frames with index
         """
-=======
-    def __init__(self,file,name:str, codec: str, width: int, height: int, duration: float, frames: int) -> None:
-        self.file = file
-        self.meta_data = 'insert by James'
-        self.name = name
-        self.codec = codec
-        self.width = int(width)
-        self.height = int(height)
-        self.duration = float(duration)
-        self.frames = int(frames)
-        self.fps = self.frames / self.duration
+
 
     def getSize(self):
         print("Video Resolution:", self.width, 'x', self.height)
@@ -61,6 +57,4 @@ class Video:
         print("Metadata for video: " + self.name)
         self.getSize()
         print("Encoding Format:", self.codec, "Duration (s):", self.duration, "with a total of", self.frames,
-              "frames! (" + str(self.fps) + " FPS)")
-
->>>>>>> 2ef4d7d6113d393ecb046274b35ace2dba5d1025
+              "frames! (" + str(self.fps) + " FPS)")   
