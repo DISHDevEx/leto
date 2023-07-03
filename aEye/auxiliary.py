00a40d69b23874290f7a6dadfbd83467175a2325
Hamza Khokhar <hamza.khokhar660@gmail.com>
2023-07-03 15:32:16 -0600

    removed static ffmpeg from auxiliary.py

--- aEye/auxiliary.py
+++ aEye/auxiliary.py
@@ -7,7 +7,7 @@ import logging
 from static_ffmpeg import run
 from aEye.processor import Processor
 
-ffmpeg, ffprobe = run.get_or_fetch_platform_executables_else_raise()
+#ffmpeg, ffprobe = run.get_or_fetch_platform_executables_else_raise()
 
 
 class Aux:
