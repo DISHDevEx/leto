666244aeb3a6a344cd7d8f0eeedd204820d0206f
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-10 07:30:38 -0600

    add load and save model

--- aEye/yolo/yolo.py
+++ aEye/yolo/yolo.py
@@ -6,11 +6,20 @@ class Yolo:
     def __init__(self) -> None:
         self.model = YOLO
         self.model_weight = None
-
+        self._s3 = boto3.client('s3')
+        
 
     def get_yolo_weight(self):
         return self.model_weight
     
+    def load_model(self, local_path = 'yolov8s.pt'):
+
+        self.model_weight = local_path
+        self.model = self.model(local_path)
+
+    def save_model(self):
+        self.model.export()
+        return self.model
 
     def train(self,data = None):
         
