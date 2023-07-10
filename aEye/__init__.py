03b5ebb9d02abd6b8cc1d11fb75b75a89f71b8a6
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-10 07:34:06 -0600

    organize init

---
+++ aEye/__init__.py
@@ -0,0 +1,8 @@
+from aEye.video import Video
+from aEye.labeler import Labeler
+from aEye.extractor import Extractor
+from aEye.auxiliary import Aux
+
+from .mediapipe import object_detection
+from .mediapipe import visualize
+
