764c2ec9548d9256f756af9622fce100f2f4fd64
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-10 07:35:14 -0600

    add init.py for yolo

--- aEye/__init__.py
+++ aEye/__init__.py
@@ -1,8 +1,13 @@
-from aEye.video import Video
-from aEye.labeler import Labeler
-from aEye.extractor import Extractor
-from aEye.auxiliary import Aux
+from .video import Video
+from .labeler import Labeler
+from .extractor import Extractor
+from .auxiliary import Aux
 
 from .mediapipe import object_detection
 from .mediapipe import visualize
+
+from .yolo import Yolo
+from .yolo import training_parameter_input
+from .yolo import prediction_parameter_input
+
 
