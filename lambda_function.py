2e4c8b5b8f93441ea7d1a334a065da6e101caa3b
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-07-12 15:19:40 -0600

    remove yolo from lambda

--- lambda_function.py
+++ lambda_function.py
@@ -35,19 +35,19 @@ def handler(event, context):
     s3_client = boto3.client('s3')
     input_video = os.path.join("/tmp", os.path.basename("Untitled.mp4"))
     mp_output_video = os.path.join("/tmp", os.path.basename("mp_output_video.mp4"))
-    yolo_output_video = os.path.join("/tmp", os.path.basename("yolo_output_video.mp4"))
+    #yolo_output_video = os.path.join("/tmp", os.path.basename("yolo_output_video.mp4"))
 
     s3_client.download_file("leto-dish", "original-videos/random-videos/demo_10_second_clip.mp4", input_video)
 
     object_detection(os.path.basename("efficientdet_lite0.tflite"), input_video, mp_output_video)
 
-    model = Yolo()
-    model.load_model_weight('yolov8s.pt')
+    #model = Yolo()
+    #model.load_model_weight('yolov8s.pt')
 
-    pipeline(input_video, model, yolo_output_video)
+    #pipeline(input_video, model, yolo_output_video)
 
     s3_client.upload_file(mp_output_video, "leto-dish", "object_detection/mp_sample.mp4")
-    s3_client.upload_file(yolo_output_video, "leto-dish", "object_detection/yolo_sample.mp4")
+    #s3_client.upload_file(yolo_output_video, "leto-dish", "object_detection/yolo_sample.mp4")
 
     reduceout = os.path.join("/tmp", os.path.basename("reduce_out.mp4"))
 
