6987ba7a6d1adfe771058132c2c40b1fb95cbb91
Pierce Lovesee <pierce.lovesee@dish.com>
2023-07-20 21:37:43 +0000

    runner function and README for fps_bitrate

---
+++ leto/reduction/fps_bitrate/app_fps_bitrate.py
@@ -0,0 +1,28 @@
+from fps_bitrate import fps_bitrate
+from aEye import Aux
+
+import sys
+import logging
+
+
+def app_fps_bitrate():
+
+    logging.info('succesfully loaded function')
+
+    aux=Aux()
+    
+    try:
+        video_list = aux.load_s3(bucket = 'aeye-data-bucket', prefix = 'input_video/')
+    except Exception as e:
+        print(e)
+        logging.warning(f"unable to load video list from s3.")
+    
+    fps_bitrate(video_list)
+    out = aux.execute_label_and_write_local(video_list)
+    aux.upload_s3(video_list, "leto-dish", "reduction/")
+    aux.clean()
+
+    return 'video reduction completed on ' + sys.version + '!'
+
+if __name__ == '__main__':
+    app_fps_bitrate()
