de76150aae11ffe4381c82a9f0969922d9816e50
vinayakSharm <vinayak.sharma@dish.com>
2023-07-21 19:56:20 +0000

    changing setup.sh file to pip install instead of conda install

--- reconstruction/realbasicvsr/reconstruction_setup.sh
+++ reconstruction/realbasicvsr/reconstruction_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 activate base
-conda install pytorch==1.7.1 torchvision==0.8.2 torchaudio==0.7.2 cudatoolkit=10.1 -c pytorch
+pip install torch torchvision torchaudio
 pip install openmim
 mim install mmcv-full
 pip install mmedit
