d744721c805d17853b3f959b41043d4f2a09eeab
JamesF2440 <James.F2440@gmail.com>
2023-07-21 11:00:09 -0600

    James Docker Test

--- downstream_model/mediapipe/setup.py
+++ downstream_model/mediapipe/setup.py
@@ -64,7 +64,8 @@ setup(
     author_email='devex@dish.com',
     license='Dish Wireless',
     packages=find_packages(
-        include=['mediapipe',
+        include=['downstream_model',
+                 'mediapipe',
                  'mediapipe.object_detection',
                  'mediapipe.visulize'
         ]
