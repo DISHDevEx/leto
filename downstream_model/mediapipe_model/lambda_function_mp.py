6a7a3e6540b2fc48aea463bff8be74e6acac6fd6
JamesF2440 <James.F2440@gmail.com>
2023-07-21 16:15:41 -0600

    James Docker Test

--- downstream_model/mediapipe_model/lambda_function_mp.py
+++ downstream_model/mediapipe_model/lambda_function_mp.py
@@ -17,11 +17,6 @@ import urllib.parse
 # input_video_path = os.environ.get('input_video_path')
 # output_video_path = os.environ.get('output_video_path')
 
-if 'LAMBDA_TASK_ROOT' in os.environ:
-    sys.path.append(f"{os.environ['LAMBDA_TASK_ROOT']}/lib")
-
-# this will render all of your packages placed as subdirs available
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 def handler(event, context):
 
