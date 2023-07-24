4bd720ae03cf3fedd617a25c3873f47343b454c4
vinayakSharm <vinayak.sharma@dish.com>
2023-07-24 16:30:59 +0000

    should be clean

--- reconstruction/realbasicvsr/reconstruction_realbasicvsr_setup.sh
+++ reconstruction/realbasicvsr/reconstruction_realbasicvsr_setup.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 <<<<<<< HEAD
+<<<<<<< HEAD
 pip install --upgrade pip
 =======
 activate base
 >>>>>>> 9c9abaa (corrected deletion of .pth file)
+=======
+pip install --upgrade pip
+>>>>>>> 6408446 (should be clean)
 pip install torch torchvision torchaudio 
 pip install openmim
 mim install mmcv-full
