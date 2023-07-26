03a882a3c0db2b045c21a39a3bdfed8f9d3692b1
Sri Harsha Vadlamudi <sriharsha219@gmail.com>
2023-07-26 09:46:24 -0600

    updated /c/Users/sriharsha.vadlamudi with /home/ssm-user

--- reduction/ffmpeg_resolution_downsampler/setup_and_run_ffmpeg_resolution_downsampler.sh
+++ reduction/ffmpeg_resolution_downsampler/setup_and_run_ffmpeg_resolution_downsampler.sh
@@ -19,13 +19,13 @@ else
     echo " Failed to install mesa-libGL"
 fi
 #Download requirements and python script files into the working directory
-if curl -o $HOME/requirements_ffmpeg_resolution_downsampler.txt https://raw.githubusercontent.com/DISHDevEx/leto/sriharsha/reduction-ffmpeg-shell-scripts/reduction/ffmpeg_resolution_downsampler/requirements_ffmpeg_resolution_downsampler.txt; then
+if curl -o /home/ssm-user/requirements_ffmpeg_resolution_downsampler.txt https://raw.githubusercontent.com/DISHDevEx/leto/sriharsha/reduction-ffmpeg-shell-scripts/reduction/ffmpeg_resolution_downsampler/requirements_ffmpeg_resolution_downsampler.txt; then
     echo "Successfully downloaded requirements_ffmpeg_resolution_downsampler.txt file"
 else
     echo "Failed to download requirements_ffmpeg_resolution_downsampler.txt file"
 fi
 #
-if curl -o $HOME/ffmpeg_resolution_downsampler.py https://raw.githubusercontent.com/DISHDevEx/leto/sriharsha/reduction-ffmpeg-shell-scripts/reduction/ffmpeg_resolution_downsampler/ffmpeg_resolution_downsampler.py; then
+if curl -o /home/ssm-user/ffmpeg_resolution_downsampler.py https://raw.githubusercontent.com/DISHDevEx/leto/sriharsha/reduction-ffmpeg-shell-scripts/reduction/ffmpeg_resolution_downsampler/ffmpeg_resolution_downsampler.py; then
     echo "Successfully downloaded ffmpeg_resolution_downsampler.py file"
 else
     echo "Failed to download ffmpeg_resolution_downsampler.py file"
@@ -37,7 +37,7 @@ else
     echo "Requirements installation failed"
 fi
 #Add libraries path to the environment variable PATH temporarily
-if export PATH=$HOME/.local/bin:$PATH; then
+if export PATH=/home/ssm-user/.local/bin:$PATH; then
     echo "Libraries are temporarily added to the PATH temporarily."
 else
     echo "Failed to add the Libraries to the PATH temporarily."
@@ -49,7 +49,7 @@ if python3 ffmpeg_resolution_downsampler.py; then
     echo "Video file destination path: https://s3.console.aws.amazon.com/s3/buckets/leto-dish?region=us-east-1&prefix=reduced-videos/benchmark/ffmpeg-resolution-downsampler/car/&showversions=false"
 fi
 #Add libraries path to the environment variable 'PATH' permanently
-if echo "export PATH=$HOME/.local/bin:$PATH;" >> ~/.bashrc; then
+if echo "export PATH=/home/ssm-user/.local/bin:$PATH;" >> ~/.bashrc; then
     echo "Libraries path is permanently added to the environment variable PATH to support recurring execution of downsampler script."
     source ~/.bashrc
 else
