e536f5d2c8f36ce1a82f3ca64d0635dc91dd1df8
luanlam97 <43329947+luanlam97@users.noreply.github.com>
2023-08-01 10:18:48 -0600

    update codebase to match

--- reduction/cv2_jpg_compress/cv2_jpg_compress.py
+++ reduction/cv2_jpg_compress/cv2_jpg_compress.py
@@ -3,6 +3,8 @@ import subprocess
 import argparse 
 import static_ffmpeg
 import cv2
+import os
+import logging
 
 def parse_args():
     """
@@ -27,7 +29,7 @@ def parse_args():
     parser.add_argument("--input_prefix_s3",
                         type = str,
                         help = "s3 prefix of the input video",
-                        default = 'reduced-videos/benchmark/ffmpeg-resolution-downsampler/car/')
+                        default = 'original-videos/benchmark/car/')
 
     parser.add_argument("--output_bucket_s3",
                         type=str,
@@ -48,6 +50,11 @@ def parse_args():
                         type = int,
                         default = 15,
                         help="The compression rate for cv2 to apply, 100 is for best video quality, 0 is for the worse video quality ")
+    
+    parser.add_argument("--crf",
+                        type = int,
+                        default = 28,
+                        help="the constant rate factor for ffmpeg to encode the video, the lower value results in higher quality. The range is 0-51")
 
     args = parser.parse_args()
 
@@ -56,7 +63,7 @@ def parse_args():
 
 def cv2_jpg_compress(video, path = "temp" , quality = 15, crf = 28):
     # Create a VideoCapture object
-    cap = cv2.VideoCapture(video.get_presigned_uel().strip("'"))
+    cap = cv2.VideoCapture(video.get_presigned_url().strip("'"))
     
     # Check if camera opened successfully
     if (cap.isOpened() == False): 
@@ -68,9 +75,10 @@ def cv2_jpg_compress(video, path = "temp" , quality = 15, crf = 28):
     frame_height = int(cap.get(4))
     fps = cap.get(cv2.CAP_PROP_FPS)
     title = video.get_title()
+    
+    os.mkdir(f'{path}_cv2')
 
-
-    out = cv2.VideoWriter(f"{path}/compressed_" + title,cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width,frame_height))
+    out = cv2.VideoWriter(f"{path}_cv2/compressed_" + title,cv2.VideoWriter_fourcc(*'mp4v'), fps, (frame_width,frame_height))
     
     index = 0
     while(True):
@@ -82,17 +90,25 @@ def cv2_jpg_compress(video, path = "temp" , quality = 15, crf = 28):
             enc = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, quality])[1]
             out.write(cv2.imdecode(enc,cv2.IMREAD_COLOR) )
             
-            index +=1 
-            print(index)
+            index +=1
     # Break the loop
         else:
             break 
-    
+    logging.info(f"successfully compressed {title} with cv2 jpeg quality rate of {quality}")
+
+
     cap.release()
     out.release()
+    print(os.system('ls'))
 
-    cmd = f"!static_ffmpeg -y -i {path}/compressed_{title} -c:v libx264  -crf {crf} -preset slow {path}/compressed_{title}"
-    subprocess.run(cmd)
+    cmd = f"static_ffmpeg -y -i {path}_cv2/compressed_{title} -c:v libx264  -crf {crf} -preset slow {path}/compressed_{title}"
+    subprocess.run(cmd, shell=True)
+
+    
+    logging.info(f"successfully reencode {title} into h264 format with the crf of {crf}")
+    
+    os.remove(f'{path}_cv2/compressed_{title}')
+    os.rmdir(f'{path}_cv2')
 
 
 def main():
@@ -105,11 +121,12 @@ def main():
 
     video_list  = aux.load_s3(args.input_bucket_s3, args.input_prefix_s3)
     for video in video_list:
-        cv2_jpg_compress(video)
+        print(video.title)
+        cv2_jpg_compress(video, args.temp_path, args.quality, args.crf)
 
-    result = aux.load_local("temp")
+    result = aux.load_local(args.temp_path) 
 
-    aux.upload_s3(result)
+    aux.upload_s3(result,args.output_bucket_s3, args.output_prefix_s3)
 
 
 if __name__ == "__main__":
