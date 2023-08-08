5be532672a5fb6e4568a436e41233f907e858a9f
Ankita Patil <patilankita2491@gmail.com>
2023-08-08 10:09:45 -0600

    Fixed import errors in encoder.py

--- reduction/deep_video_reduction/Encoder.py
+++ reduction/deep_video_reduction/Encoder.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser
 
 
 def load_graph(frozen_graph_filename):
-    with tf.gfile.GFile(frozen_graph_filename, "rb") as f:
+    with tf.io.gfile.GFile(frozen_graph_filename, "rb") as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
 
