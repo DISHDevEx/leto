6b1ce2b7b46821a55db39b548493de399d2bef58
Ankita Patil <patilankita2491@gmail.com>
2023-08-08 16:41:36 -0600

    Fixed decoder.py errors

--- reduction/deep_video_reduction/decoder.py
+++ reduction/deep_video_reduction/decoder.py
@@ -2,16 +2,8 @@ import joblib
 import tensorflow as tf
 from imageio import imread
 from argparse import ArgumentParser
-import math
 import numpy as np
 import cv2
-
-def CalcuPSNR(target, ref):
-    diff = ref - target
-    diff = diff.flatten('C')
-    rmse = math.sqrt(np.mean(diff**2.))
-    return 20 * math.log10(1.0 / (rmse))
-
 
 def load_graph(frozen_graph_filename):
     with tf.io.gfile.GFile(frozen_graph_filename, "rb") as f:
@@ -25,11 +17,11 @@ def load_graph(frozen_graph_filename):
 
 def decoder(loadmodel, refer_path, outputfolder):
     graph = load_graph(loadmodel)
-
-    reconframe = graph.get_tensor_by_name('import/build_towers/tower_0/train_net_inference_one_pass/train_net/ReconFrame:0')
-    res_input = graph.get_tensor_by_name('import/quant_feature:0')
-    res_prior_input = graph.get_tensor_by_name('import/quant_z:0')
-    motion_input = graph.get_tensor_by_name('import/quant_mv:0')
+    prefix = 'import/build_towers/tower_0/train_net_inference_one_pass/train_net/'
+    reconframe = graph.get_tensor_by_name(prefix + 'ReconFrame:0')
+    res_input = graph.get_tensor_by_name(prefix + 'quant_feature:0')
+    res_prior_input = graph.get_tensor_by_name(prefix + 'quant_z:0')
+    motion_input = graph.get_tensor_by_name(prefix + 'quant_mv:0')
     previousImage = graph.get_tensor_by_name('import/input_image_ref:0')
 
     with tf.compat.v1.Session(graph=graph) as sess:
@@ -43,9 +35,9 @@ def decoder(loadmodel, refer_path, outputfolder):
         with open(outputfolder + 'quantized_motion_feature.pkl', 'rb') as f:
             motion_feature = joblib.load(f)
 
-        im1 = imread(refer_path)
-        im1 = im1 / 255.0
-        im1 = np.expand_dims(im1, axis=0)
+        im2 = imread(refer_path)
+        im2 = im2 / 255.0
+        im2 = np.expand_dims(im2, axis=0)
 
         # reconstructed image
         recon_d = sess.run(
@@ -54,18 +46,11 @@ def decoder(loadmodel, refer_path, outputfolder):
                 res_input: residual_feature,
                 res_prior_input: residual_prior_feature,
                 motion_input: motion_feature,
-                previousImage: im1
+                previousImage: im2
             })
 
         reconstructed_image = np.squeeze(recon_d)
-        cv2.imwrite(outputfolder + 'input.jpg', im1)
-
-        # check
-        # imagedir = './image/'
-        # im2 = imread(imagedir + 'im003.png')
-        # im2 = im2 / 255.0
-        # im2 = np.expand_dims(im2, axis=0)
-        # print(CalcuPSNR(im2, recon_d))
+        cv2.imwrite(outputfolder + 'output.png', reconstructed_image)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
