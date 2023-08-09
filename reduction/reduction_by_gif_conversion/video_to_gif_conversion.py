47c54cbfcad277cff8e8ff447a73871ff61c1c87
tamanna.kawatra <tamanna.kawatra@dish.com>
2023-08-09 17:23:40 -0600

    adding reduction gif conversion

---
+++ reduction/reduction_by_gif_conversion/video_to_gif_conversion.py
@@ -0,0 +1,18 @@
+import ffmpeg
+import subprocess
+
+# Replace these with your actual input and output file paths
+input_video_path = "/Users/tamanna.kawatra/Desktop/video_streaming_analytics/Video_Benchmark_car.mp4"
+output_gif_path = "car_output_30fps_640p.gif"
+
+# Run the ffmpeg command to convert video to GIF
+command = [
+    "static_ffmpeg",
+    "-i", input_video_path,
+    "-vf", "fps=10,scale=640:-1:flags=lanczos",
+    "-c:v", "gif",
+    output_gif_path
+]
+
+# Run the command
+subprocess.run(command)
