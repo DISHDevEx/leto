54c1443b98ec0ec24e29806b095c7212a546a364
tamanna.kawatra <tamanna.kawatra@dish.com>
2023-08-09 17:25:53 -0600

    changes

--- reduction/reduction_by_gif_conversion/video_to_gif_conversion.py
+++ reduction/reduction_by_gif_conversion/video_to_gif_conversion.py
@@ -1,6 +1,14 @@
 import ffmpeg
 import subprocess
 
+from aEye import Video
+from aEye import Labeler
+from aEye import Aux
+import sys
+import logging
+import argparse
+
+
 # Replace these with your actual input and output file paths
 input_video_path = "/Users/tamanna.kawatra/Desktop/video_streaming_analytics/Video_Benchmark_car.mp4"
 output_gif_path = "car_output_30fps_640p.gif"
