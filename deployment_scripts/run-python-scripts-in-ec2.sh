3470828ddbb73534f4d19531fadbcbfeea24eb1a
Sri Harsha Vadlamudi <sriharsha219@gmail.com>
2023-08-14 09:22:45 -0600

    added new conda and source commands

--- deployment_scripts/run-python-scripts-in-ec2.sh
+++ deployment_scripts/run-python-scripts-in-ec2.sh
@@ -3,9 +3,11 @@
 #
 #!/bin/bash
 #Set variable values
+source ~/.bashrc
 WORKING_DIRECTORY="/home/ec2-user/leto"
 FILE_PATH=$1
 cd $WORKING_DIRECTORY
 #Run the python script
 conda activate leto
+conda env list
 python3 $WORKING_DIRECTORY/$1
