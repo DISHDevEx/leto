f2bf6707789867c88e3508f4d00971cdcd90a193
Sri Harsha Vadlamudi <sriharsha219@gmail.com>
2023-08-14 09:53:19 -0600

    corrected typ in PATH command

--- deployment_scripts/run-python-scripts-in-ec2.sh
+++ deployment_scripts/run-python-scripts-in-ec2.sh
@@ -3,7 +3,7 @@
 #
 #!/bin/bash
 #Set variable values
-export PATH="$HOME/miniconda3/bin:$PATH
+export PATH="$HOME/miniconda3/bin:$PATH"
 conda activate leto
 conda env list
 WORKING_DIRECTORY="/home/ec2-user/leto"
