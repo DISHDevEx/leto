866b4b8919f7f906ed3f576d07e9e060c8f1bc6a
PierceLovesee <pierce.lovesee@dish.com>
2023-08-16 01:12:02 +0000

    realbasicvsr

--- reconstruction/realbasicvsr/reconstruction_realbasicvsr.py
+++ reconstruction/realbasicvsr/reconstruction_realbasicvsr.py
@@ -24,6 +24,7 @@ root_path = subprocess.run(
 sys.path.append(root_path)
 
 from utilities import CloudFunctionality
+from utilities import ConfigHandler
 
 VIDEO_EXTENSIONS = (".mp4", ".mov")
 
@@ -134,12 +135,9 @@ def realbasicvsr_runner(method_args):
 if __name__ == "__main__":
     cloud_functionality = CloudFunctionality()
 
-    # load and allocate config file
-    config = configparser.ConfigParser(inline_comment_prefixes=';')
-    config.read('../../config.ini')
-    s3_args = config['DEFAULT']
-    method_args = config['reconstruction.recon_args']
-    logging.info("successfully loaded config file")
+    config = ConfigHandler('[reconstruction.realbasicvsr')
+    s3_args = config.s3
+    method_args = config.method
 
     cloud_functionality.preprocess(method_args, s3_args)
 
