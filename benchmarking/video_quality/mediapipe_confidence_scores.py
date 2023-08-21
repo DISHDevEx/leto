14e2307a974de3063959e8a8b8885401fbbd5b14
PierceLovesee <pierce.lovesee@dish.com>
2023-08-21 19:01:25 +0000

    refactor mediapipe_confidence_scores to use config.ini

--- benchmarking/video_quality/mediapipe_confidence_scores.py
+++ benchmarking/video_quality/mediapipe_confidence_scores.py
@@ -1,24 +1,41 @@
+import subprocess
+import sys
 import boto3
 import json
 
+# get git repo root level
+root_path = subprocess.run(
+    ["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True, check=False
+).stdout.rstrip("\n")
+
+# add git repo path to use all libraries
+sys.path.append(root_path)
+
+from utilities import ConfigHandler
+
+config = ConfigHandler('benchmarking.mp_confidence')
+s3 = config.s3
+method = config.method
+
+
 # Create a Lambda client
-lambda_client = boto3.client('lambda', region_name='your_region')
+lambda_client = boto3.client('lambda', region_name=s3['region'])
 
 # Lambda function name
 
-function_name = 'your_lambda_function_name'
+function_name = method['function_name']
 
 # Payload data
 payload = {
-    "bucket_name": "leto-dish",
-    "folder_path": "og-videos/benchmark/violencedetection/",
-    "dynamodb_table" : "test_table"
+    "bucket_name": s3['input_bucket_s3'],
+    "folder_path": method['folder_path'],
+    "dynamodb_table" : method['dynamodb_table']
 }
 
 # Call the Lambda function
 response = lambda_client.invoke(
     FunctionName=function_name,
-    InvocationType='RequestResponse',  # Use 'Event' for asynchronous invocation
+    InvocationType=method['invocation_type'], 
     Payload=json.dumps(payload)
 )
 
