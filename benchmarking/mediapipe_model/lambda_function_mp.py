9b6315f892fed74fa4b2ccc283a183b6ac71a577
Hamza Khokhar <hamza.khokhar660@gmail.com>
2023-08-21 11:22:08 -0600

    updated mediapipe model to reflect dynamodb changes

--- benchmarking/mediapipe_model/lambda_function_mp.py
+++ benchmarking/mediapipe_model/lambda_function_mp.py
@@ -1,11 +1,12 @@
 """
-Module to contain the handler function for the YOLO lambda function. This file is what the lambda functions runs.
+Module to contain the handler function for the MediaPipe lambda function. This file is what the lambda functions runs.
 """
+import os
+from object_detection import object_detection
+
 import boto3
 import os
-from yolo import Yolo
 
-from pipeline import pipeline
 import urllib.parse
 
 s3_client = boto3.client("s3")
@@ -13,7 +14,7 @@ dynamodb = boto3.resource('dynamodb')
 
 def handler(event, context):
     """
-    Current version of the lambda function reads from an s3 bucket and applies yolo's object detection model on the files.
+    Current version of the lambda function reads from an s3 bucket and applies mediapipe's object detection model on the files.
 
     Parameters
     ----------
@@ -24,9 +25,7 @@ def handler(event, context):
     print(os.system("ls"))
 
     score = {}
-
-    yolo_model = Yolo()
-    yolo_model.load_model_weight("yolov8s.pt")
+    mp_model = os.path.basename("efficientdet_lite0.tflite")
 
     bucket_name = event["bucket_name"]
     folder_path = event["folder_path"]
@@ -37,20 +36,21 @@ def handler(event, context):
         s3_keys = list_object_keys(bucket_name, folder_path)
 
         for key in s3_keys:
-
             # Download the video from S3 to Lambda's /tmp directory
             local_filename = '/tmp/' + os.path.basename(key)
             s3_client.download_file(bucket_name, key, local_filename)
 
             # Process the downloaded video
 
-            mAC = pipeline(local_filename, yolo_model, "")
+            mAC = object_detection(mp_model,local_filename, "")
             if len(mAC):
-                mean_average_confidence = sum(mAC.values()) / len(mAC)
+                mean_average_confidence = sum(mAC.values()) / len(
+                    mAC)
 
-
+            # Optionally, you can delete the local file to save space
             os.remove(local_filename)
             score.update({key: mean_average_confidence})
+
             table_name = dynamodb_table
 
             table = dynamodb.Table(table_name)
@@ -59,9 +59,10 @@ def handler(event, context):
                 print('Uploaded location:', key)
             except Exception as e:
                 print('Error uploading metric:', mean_average_confidence, e)
-            print({key: mean_average_confidence})
 
+            print({key: mean_average_confidence})
         return score
+
 
     except Exception as e:
         print(e)
