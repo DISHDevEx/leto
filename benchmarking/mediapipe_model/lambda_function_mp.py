316c54afeddd9babd00a5ff590c99b49faf55f44
Hamza Khokhar <hamza.khokhar660@gmail.com>
2023-08-21 11:16:23 -0600

    updated mediapipe model to reflect dynamodb changes

--- benchmarking/mediapipe_model/lambda_function_mp.py
+++ benchmarking/mediapipe_model/lambda_function_mp.py
@@ -1,19 +1,19 @@
 """
-Module to contain the handler function for the MediaPipe lambda function. This file is what the lambda functions runs.
+Module to contain the handler function for the YOLO lambda function. This file is what the lambda functions runs.
 """
-import os
-from object_detection import object_detection
-
 import boto3
 import os
+from yolo import Yolo
 
+from pipeline import pipeline
 import urllib.parse
 
 s3_client = boto3.client("s3")
+dynamodb = boto3.resource('dynamodb')
 
 def handler(event, context):
     """
-    Current version of the lambda function reads from an s3 bucket and applies mediapipe's object detection model on the files.
+    Current version of the lambda function reads from an s3 bucket and applies yolo's object detection model on the files.
 
     Parameters
     ----------
@@ -24,7 +24,9 @@ def handler(event, context):
     print(os.system("ls"))
 
     score = {}
-    mp_model = os.path.basename("efficientdet_lite0.tflite")
+
+    yolo_model = Yolo()
+    yolo_model.load_model_weight("yolov8s.pt")
 
     bucket_name = event["bucket_name"]
     folder_path = event["folder_path"]
@@ -35,32 +37,31 @@ def handler(event, context):
         s3_keys = list_object_keys(bucket_name, folder_path)
 
         for key in s3_keys:
+
             # Download the video from S3 to Lambda's /tmp directory
             local_filename = '/tmp/' + os.path.basename(key)
             s3_client.download_file(bucket_name, key, local_filename)
 
             # Process the downloaded video
 
-            mAC = object_detection(mp_model,local_filename, "")
+            mAC = pipeline(local_filename, yolo_model, "")
             if len(mAC):
-                mean_average_confidence = sum(mAC) / len(
-                    mAC)
-            video_location = f's3://{bucket_name}/{key}'
-            # Optionally, you can delete the local file to save space
+                mean_average_confidence = sum(mAC.values()) / len(mAC)
+
+
             os.remove(local_filename)
-            score.update({video_location: mean_average_confidence})
-            print({video_location: mean_average_confidence})
+            score.update({key: mean_average_confidence})
+            table_name = dynamodb_table
+
+            table = dynamodb.Table(table_name)
+            try:
+                response = table.put_item(Item={'video_location':  key, 'score': str(mean_average_confidence)})
+                print('Uploaded location:', key)
+            except Exception as e:
+                print('Error uploading metric:', mean_average_confidence, e)
+            print({key: mean_average_confidence})
+
         return score
-
-
-
-
-        mAC = object_detection(mp_model, video.get_file().strip("'"), mp_output_video)
-
-        video_location = f's3://{bucket}/{key}'
-
-        return {video_location : mAC }
-
 
     except Exception as e:
         print(e)
@@ -77,4 +78,4 @@ def list_object_keys(bucket_name, folder_path):
     if 'Contents' in response:
         for obj in response['Contents']:
             keys.append(obj['Key'])
-    return keys[1:-1]
+    return keys[1:]
