b6d2c63ef4ac77e020cc8ef7d22ae70badafe562
Hamza Khokhar <hamza.khokhar660@gmail.com>
2023-08-22 12:02:55 -0600

    added doc strings and updated the README.md

--- benchmarking/video_quality/yolo_confindence_scores.py
+++ benchmarking/video_quality/yolo_confindence_scores.py
@@ -4,46 +4,72 @@ import boto3
 import botocore.config
 import json
 
-# get git repo root level
-root_path = subprocess.run(
-    ["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True, check=False
-).stdout.rstrip("\n")
+def invoke_lambda_and_process_response():
+    """
+    Invokes a Lambda function with specified payload and processes the response.
 
-# add git repo path to use all libraries
-sys.path.append(root_path)
+    This script demonstrates how to invoke an AWS Lambda function with a payload,
+    wait for the response, and process the returned payload.
 
-from utilities import ConfigHandler
+    Note:
+    - Before using this script, ensure that the required AWS credentials and configurations
+      are properly set up, and that the 'utilities' module and its dependencies are accessible
+      in the specified git repository.
 
-config = ConfigHandler('benchmarking.yolo_confidence')
-s3 = config.s3
-method = config.method
+    Returns:
+        None
 
-# Create a Lambda client
-config = botocore.config.Config(
-    read_timeout=900,
-    connect_timeout=900,
-    retries={"max_attempts": 0}
-)
-lambda_client = boto3.client('lambda', region_name=s3['region'], config=config)
+    Raises:
+        subprocess.CalledProcessError: If there is an error while executing the git command.
+        botocore.exceptions.ConfigError: If there is an error in the AWS SDK configuration.
+        botocore.exceptions.WaiterError: If waiting for the Lambda function response encounters an error.
+        json.JSONDecodeError: If there is an error while decoding the Lambda response payload.
 
-# Lambda function name
+    """
+    # get git repo root level
+    root_path = subprocess.run(
+        ["git", "rev-parse", "--show-toplevel"], capture_output=True, text=True, check=False
+    ).stdout.rstrip("\n")
 
-function_name = method['function_name']
+    # add git repo path to use all libraries
+    sys.path.append(root_path)
 
-# Payload data
-payload = {
-    "bucket_name": s3['input_bucket_s3'],
-    "folder_path": method['folder_path'],
-    "dynamodb_table" : method['dynamodb_table']
-}
+    from utilities import ConfigHandler
 
-# Call the Lambda function
-response = lambda_client.invoke(
-    FunctionName=function_name,
-    InvocationType='RequestResponse',  # Use 'Event' for asynchronous invocation
-    Payload=json.dumps(payload)
-)
+    # Load configuration using ConfigHandler
+    config = ConfigHandler('benchmarking.yolo_confidence')
+    s3 = config.s3
+    method = config.method
 
-# Process the response
-response_payload = json.loads(response['Payload'].read().decode('utf-8'))
-print("Lambda Response:", response_payload)
+    # Create a Lambda client with custom configuration
+    config = botocore.config.Config(
+        read_timeout=900,
+        connect_timeout=900,
+        retries={"max_attempts": 0}
+    )
+    lambda_client = boto3.client('lambda', region_name=s3['region'], config=config)
+
+    # Lambda function name
+    function_name = method['function_name']
+
+    # Payload data
+    payload = {
+        "bucket_name": s3['input_bucket_s3'],
+        "folder_path": method['folder_path'],
+        "dynamodb_table": method['dynamodb_table']
+    }
+
+    # Call the Lambda function
+    response = lambda_client.invoke(
+        FunctionName=function_name,
+        InvocationType='RequestResponse',  # Use 'Event' for asynchronous invocation
+        Payload=json.dumps(payload)
+    )
+
+    # Process the response
+    response_payload = json.loads(response['Payload'].read().decode('utf-8'))
+    print("Lambda Response:", response_payload)
+
+# Invoke the function when this script is run
+if __name__ == "__main__":
+    invoke_lambda_and_process_response()
