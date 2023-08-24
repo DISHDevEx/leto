000d3330e781952a901f772acedea076078e050a
Monisha Gnanaprakasam <monisha.gnanaprakasa@dish.com>
2023-08-24 14:32:44 -0600

    Duplication issue fixed

--- benchmarking/video_quality/table_merge.py
+++ benchmarking/video_quality/table_merge.py
@@ -1,32 +1,30 @@
 import boto3
 import pandas as pd
 
-dynamodb = boto3.resource('dynamodb')
+dynamodb = boto3.client('dynamodb')
 
 def mergetablestodf():
-    """
-        Merge dynamodb tables into a dataframe
-
-        Returns:
-        ----------
-            df: The dataframe with combined data from dynamoDB tablesß
-    """
-
     # Define table names
     table_names = ['leto_original_file_size', 'leto_reduced_file_size', 'leto_reconstructed_file_size','leto_yolo','leto_mediapipe']  # Add your table names here
 
     # Retrieve data from each table
-    table_data = []
-    for table_name in table_names:
-        table = dynamodb.Table(table_name)
-        response = table.scan()
-        #ßresponse = dynamodb.scan(TableName=table_name)
-        items = response.get('Items', [])
-        table_data.extend(items)
-        
-    # Convert DynamoDB data to a pandas DataFrame
-    df = pd.DataFrame(table_data)
 
+    dataframes = pd.DataFrame()
+    for table_name in table_names:
+        response = dynamodb.scan(TableName=table_name)
+        items = response.get('Items',[])
+        table_data = []
+        for item in items:
+            row = {key: val['S'] for key, val in item.items()}    
+            table_data.append(row)    
+        # Convert DynamoDB data to a pandas DataFrame
+        df = pd.DataFrame(table_data)
+    
+        if len(dataframes) ==0:
+            dataframes = df
+        else:
+            dataframes=pd.merge(dataframes,df,on='video_location',how='outer')
+        df = pd.DataFrame()
     return df
 
 def main():
