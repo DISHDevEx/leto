3ed6e768d2df8b4c9eaa5075abcd7028d726a7e9
tamanna.kawatra <tamanna.kawatra@dish.com>
2023-08-29 14:05:02 -0600

    changes with config.ini

--- reduction/keyframe_extraction/keyframe_extraction.py
+++ reduction/keyframe_extraction/keyframe_extraction.py
@@ -22,17 +22,20 @@ from utilities import ConfigHandler
 from utilities import CloudFunctionality
 
 
-# Load the model 
+# Load the model
 cnn_model = resnet50(pretrained=True)
 cnn_model = cnn_model.eval()
 
+
 def extract_frame_features(frame):
-    transform = transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize((224, 224)),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    ])
+    transform = transforms.Compose(
+        [
+            transforms.ToPILImage(),
+            transforms.Resize((224, 224)),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ]
+    )
 
     frame = transform(frame)
     frame = torch.unsqueeze(frame, 0)
@@ -43,15 +46,18 @@ def extract_frame_features(frame):
 
     return features.flatten().numpy()
 
-def extract_key_frames(video_list, path ='temp', num_key_frames=30):
+
+def extract_key_frames(video_list, path="temp", num_key_frames=30):
     for video in video_list:
-        video_path = os.path.join('/root/PyTorchVideoCompression/original_videos/',video)
+        video_path = os.path.join(
+            "/root/PyTorchVideoCompression/original_videos/", video
+        )
         cap = cv2.VideoCapture(video_path)
         video_name = Path(video_path).stem
         frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = int(frame_count/num_key_frames)
+        fps = int(frame_count / num_key_frames)
         frames = []
 
         # Extract features from each frame
@@ -69,7 +75,9 @@ def extract_key_frames(video_list, path ='temp', num_key_frames=30):
         cluster_centers = kmeans.cluster_centers_
 
         # Calculate the distance of each frame to the cluster centers
-        distances = np.linalg.norm(np.array(frames)[:, np.newaxis] - cluster_centers, axis=2)
+        distances = np.linalg.norm(
+            np.array(frames)[:, np.newaxis] - cluster_centers, axis=2
+        )
         # Get the index of the closest cluster center for each frame
         closest_clusters = np.argmin(distances, axis=1)
 
@@ -77,19 +85,23 @@ def extract_key_frames(video_list, path ='temp', num_key_frames=30):
         key_frames = []
         for cluster_idx in range(num_key_frames):
             cluster_frames = np.where(closest_clusters == cluster_idx)[0]
-            representative_frame_idx = cluster_frames[np.argmax(distances[cluster_frames, cluster_idx])]
+            representative_frame_idx = cluster_frames[
+                np.argmax(distances[cluster_frames, cluster_idx])
+            ]
             key_frames.append(representative_frame_idx)
 
-         # make video from frames 
+        # make video from frames
         fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-        output_path = os.path.join(path, video_name+'_keyframes.mp4')
+        output_path = os.path.join(path, video_name + "_keyframes.mp4")
         print(output_path)
-        out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height), isColor=True)
+        out = cv2.VideoWriter(
+            output_path, fourcc, fps, (frame_width, frame_height), isColor=True
+        )
         video_stream = cv2.VideoCapture(video_path)
         key_frames.sort()
-        #print(key_frames)
+        # print(key_frames)
         for i in key_frames:
-            #print(i)
+            # print(i)
             video_stream.set(cv2.CAP_PROP_POS_FRAMES, i)
 
             ret, frame = video_stream.read()
@@ -99,15 +111,10 @@ def extract_key_frames(video_list, path ='temp', num_key_frames=30):
             out.write(frame)
         video_stream.release()
         out.release()
-        encoded_video_name = os.path.join(path, video_name+'.mp4')
+        encoded_video_name = os.path.join(path, video_name + ".mp4")
         cmd = f"static_ffmpeg -y -i {output_path} -c:v libx264  -crf 34 -preset veryfast {encoded_video_name}"
         subprocess.run(cmd, shell=True)
         os.remove(output_path)
-
-        
-    
-
-
 
 
 def main():
@@ -118,11 +125,10 @@ def main():
     cloud_functionality = CloudFunctionality()
 
     video_list = cloud_functionality.preprocess_reduction(s3_args, method_args)
-    extract_key_frames(video_list ,method_args["temp_path"], method_args.getint('num_key_frames') )
+    extract_key_frames(
+        video_list, method_args["temp_path"], method_args.getint("num_key_frames")
+    )
     cloud_functionality.postprocess_reduction(s3_args, method_args)
-
-
-    
 
 
 if __name__ == "__main__":
