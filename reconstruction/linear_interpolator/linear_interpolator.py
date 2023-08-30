6d01aaa8b16a5b0728bb34983c38b7a6259998c9
tamanna.kawatra <tamanna.kawatra@dish.com>
2023-08-29 20:38:12 -0600

    spelling error

--- reconstruction/linear_interpolator/linear_interpolator.py
+++ reconstruction/linear_interpolator/linear_interpolator.py
@@ -20,7 +20,7 @@ def linear_interpolation(frame_a, frame_b, alpha):
     return cv2.addWeighted(frame_a, 1 - alpha, frame_b, alpha, 0)
 
 def reconstruct_video_with_keyframe_images(video_list, target_frame_rate= 30):
-        path = "./recontructed_videos"
+        path = "./reconstructed_videos"
         for video in video_list:
             video_path = video.get_file().strip("'")
             cap = cv2.VideoCapture(video_path)
