b75b115d442bdcc4192558cd301e26586a3d3257
tamanna.kawatra <tamanna.kawatra@dish.com>
2023-09-05 10:18:46 -0600

    reformatting using black

--- reconstruction/linear_interpolator/linear_interpolator.py
+++ reconstruction/linear_interpolator/linear_interpolator.py
@@ -36,7 +36,7 @@ def linear_interpolation(frame_a, frame_b, alpha):
     - interpolated_frame: The linearly interpolated image frame (numpy.ndarray)
     """
     return cv2.addWeighted(frame_a, 1 - alpha, frame_b, alpha, 0)
-    
+
 
 def reconstruct_video_with_keyframe_images(target_frame_rate=30):
     """
