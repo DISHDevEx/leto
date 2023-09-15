092243e5219202bb8ab4a8db85c9ec92d7bc245d
PierceLovesee <pierce.lovesee@dish.com>
2023-09-15 17:58:05 +0000

    add docstrings

--- utilities/cloud_functionality_reconstruction.py
+++ utilities/cloud_functionality_reconstruction.py
@@ -17,10 +17,36 @@ class CloudFunctionalityReconstruction:
         self.method_args = method_args
 
     def __enter__(self):
+        """
+        Enter method for context management.
+
+        This method is automatically invoked when using the 'with' statement to instantiate
+        the CloudFunctionalityReconstruction class. It logs the instantiation and returns
+        the instance itself for context management.
+
+        Returns:
+        CloudFunctionalityReconstruction: The instance of the CloudFunctionalityReconstruction class.
+        """
         logging.info('CloudFunctionalityReconstruction class instantiated')
         return self
     
     def __exit__(self, exc_type, exc_value, tb):
+        """
+        Exit method for context management.
+
+        This method is automatically invoked when exiting the 'with' statement that
+        encapsulates the CloudFunctionalityReconstruction class. It handles any exceptions
+        that may have occurred during the context, prints a traceback if an exception is
+        raised, and performs post-processing of video reconstruction using 'postprocess_reconstruction'.
+
+        Parameters:
+        exc_type (type): The type of exception, if any.
+        exc_value (Exception): The exception instance, if any.
+        tb (traceback): The traceback object, if any.
+
+        Returns:
+        bool: True to indicate that the exception, if any, has been handled.
+        """
         if exc_type is not None:
             traceback.print_exception(exc_type, exc_value, tb)
         self.postprocess_reconstruction(self.method_args)
@@ -55,6 +81,23 @@ class CloudFunctionalityReconstruction:
 
 
     def upload_reconstruction(self, s3_args, method_args):
+        """
+        Upload reconstructed video files to an S3 bucket.
+
+        This method loads the reconstructed video files from the local 'reconstructed_videos' directory,
+        which is assumed to be at "./reconstructed_videos". It then uploads these video files to an S3
+        bucket specified by 'output_bucket_s3' in 's3_args', using the 'output_prefix_s3' from 'method_args'
+        to define the object's path.
+
+        Parameters:
+        s3_args (dict): A dictionary containing S3-related configuration.
+            - output_bucket_s3 (str): The name of the S3 bucket to upload to.
+        method_args (dict): A dictionary containing method-specific configuration.
+            - output_prefix_s3 (str): The prefix for the S3 object path.
+
+        Returns:
+        None
+        """        
         # Load reconstructed video files
         reconstructed_video_list = self.aux.load_local("./reconstructed_videos")
 
@@ -66,6 +109,19 @@ class CloudFunctionalityReconstruction:
         )
 
     def postprocess_reconstruction(self, method_args):
+        """
+        Perform post-processing tasks after video reconstruction.
+
+        This method cleans up the local 'reconstructed_videos' directory, which is assumed to be located
+        at "./reconstructed_videos", by deleting it. This is typically done after the video reconstruction
+        process to remove temporary or intermediate files.
+
+        Parameters:
+        method_args (dict): A dictionary containing method-specific configuration (not used in this method).
+
+        Returns:
+        None
+        """
 
         # Delete reconstructed_videos folder from local
         self.aux.set_local_path("./reconstructed_videos")
--- utilities/cloud_functionality_reduction.py
+++ utilities/cloud_functionality_reduction.py
@@ -16,10 +16,36 @@ class CloudFunctionalityReduction:
         self.method_args = method_args
 
     def __enter__(self):
+        """
+        Enter method for context management.
+
+        This method is automatically invoked when using the 'with' statement to instantiate
+        the CloudFunctionalityReduction class. It logs the instantiation and returns
+        the instance itself for context management.
+
+        Returns:
+        CloudFunctionalityReduction: The instance of the CloudFunctionalityReduction class.
+        """
         logging.info('CloudFunctionalityReduction class instantiated')
         return self
     
     def __exit__(self, exc_type, exc_value, tb):
+        """
+        Exit method for context management.
+
+        This method is automatically invoked when exiting the 'with' statement that
+        encapsulates the CloudFunctionalityReduction class. It handles any exceptions
+        that may have occurred during the context and performs post-processing reduction.
+
+        Parameters:
+        exc_type (type): The type of exception, if any.
+        exc_value (Exception): The exception instance, if any.
+        tb (traceback): The traceback object, if any.
+
+        Returns:
+        bool: True to indicate that the exception, if any, has been handled.
+        """
+        
         if exc_type is not None:
             traceback.print_exception(exc_type, exc_value, tb)
         self.postprocess_reduction(self.method_args)
@@ -49,12 +75,41 @@ class CloudFunctionalityReduction:
         return video_list
 
     def upload_reduction(self, s3_args, method_args):
-        
+        """
+        Upload the reduction results to an S3 bucket.
+
+        This method loads the reduction results from a local path specified in 'temp_path'
+        within 'method_args' and uploads them to an S3 bucket specified by 'output_bucket_s3'
+        in 's3_args', using the 'output_prefix_s3' from 'method_args' to define the object's path.
+
+        Parameters:
+        s3_args (dict): A dictionary containing S3-related configuration.
+            - output_bucket_s3 (str): The name of the S3 bucket to upload to.
+        method_args (dict): A dictionary containing method-specific configuration.
+            - temp_path (str): The local path where the reduction results are stored.
+            - output_prefix_s3 (str): The prefix for the S3 object path.
+
+        Returns:
+        None
+        """
         result = self.aux.load_local(method_args['temp_path'])
         self.aux.upload_s3(result, bucket=s3_args['output_bucket_s3'], prefix=method_args['output_prefix_s3'])
 
 
     def postprocess_reduction(self, method_args):
+        """
+        Perform post-processing tasks on the reduction results.
+
+        This method sets the local path to the one specified in 'temp_path' within 'method_args'
+        and then cleans up any temporary files or resources related to the reduction process.
+
+        Parameters:
+        method_args (dict): A dictionary containing method-specific configuration.
+            - temp_path (str): The local path where the reduction results are stored.
+
+        Returns:
+        None
+        """
 
         self.aux.set_local_path(method_args['temp_path'])
         self.aux.clean()
